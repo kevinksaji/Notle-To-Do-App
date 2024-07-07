23ce96b976b289f4aaf049d78d3a3d13a72d4e17
kevinksaji <kevink.saji.2022@scis.smu.edu.sg>
2024-07-08 03:16:16 +0800

    created basic functionality of to-do application both server side and client side code completed in index.js and index.html respectively

---
+++ api/index.js
@@ -0,0 +1,43 @@
+var Express = require('express');
+var Mongoclient = require('mongodb').MongoClient;
+var cors = require('cors');
+const multer = require('multer');
+
+var app = Express();
+app.use(cors());
+
+var CONNECTION_STRING = "mongodb+srv://admin:admin@cluster0.pmhjpot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+var DATABASENAME="todoappdb";
+var database;
+
+app.listen(5038, () => {
+    Mongoclient.connect(CONNECTION_STRING, (error, client) => {
+        database = client.db(DATABASENAME);
+        console.log("Connected to `" + DATABASENAME + "`!");
+    });
+} );
+
+app.get("/api/todoapp/GetNotes", (request, response) => {
+    database.collection("todoappcollection").find({}).toArray((error, result)=>{
+        response.send(result);
+    });
+})
+
+app.post("/api/todoapp/AddNotes", multer().none(), (request, response) => {
+    database.collection("todoappcollection").count({}, function(error,numOfDocs){
+        database.collection("todoappcollection").insertOne({
+            id: (numOfDocs + 1).toString(),
+            description: request.body.newNotes
+    });
+    response.send("Added successfully");
+    });
+})
+
+app.delete('/api/todoapp/DeleteNotes', (request, response) => {
+    database.collection("todoappcollection").deleteOne({
+        id: request.query.id
+    });
+    response.send("Deleted successfully");
+})
